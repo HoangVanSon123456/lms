8e23c3b1150061eef3bb41cd26321aa8a89f3e49
SonHoangApusCorp <son.hoangvan@apuscorp.com>
2025-07-09 18:00:10 +0700

    update

---
+++ src/main/java/com/example/lms/entity/Role.java
@@ -0,0 +1,21 @@
+package com.example.lms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name; // ADMIN, TEACHER, STUDENT
+}
