1ccfa483b2d64f121183f5551a9049d3bb6c3ae6
SonHoangApusCorp <son.hoangvan@apuscorp.com>
2025-07-09 17:41:51 +0700

    update

---
+++ src/main/java/com/example/lms/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.lms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.security.Timestamp;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @Column(name = "is_locked")
+    private Boolean isLocked = false;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "update_at")
+    private Timestamp updateAt;
+}
+
+
