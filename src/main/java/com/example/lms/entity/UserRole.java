d39860e2ee97274a6a373c16c92e6568f81eb2d5
SonHoangApusCorp <son.hoangvan@apuscorp.com>
2025-07-11 15:16:56 +0700

    upadte

--- src/main/java/com/example/lms/entity/UserRole.java
+++ src/main/java/com/example/lms/entity/UserRole.java
@@ -1,4 +1,17 @@
 package com.example.lms.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+
+@Entity
+@IdClass(UserRoleId.class)
 public class UserRole {
+
+    @Id
+    private Integer userId;
+
+    @Id
+    private Integer roleId;
+
 }
